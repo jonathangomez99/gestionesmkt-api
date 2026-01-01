c1cef3a0247d56154bf3ab5f4922bda0b48aafcb
Jonathan Gomez <jo.gomez1111@gmail.com>
2026-01-01 16:54:31 -0300

    Setup database, SQLAlchemy session and Alembic migrations

---
+++ db/engine.py
@@ -0,0 +1,17 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(
+    DATABASE_URL,
+    echo = os.getenv("SQL_ECHO", "false").lower() in ("1", "true", "yes"),
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
--- db/session.py
+++ db/session.py
@@ -1,16 +1,9 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from .engine import SessionLocal
 
-DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
