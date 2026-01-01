a7569ec1284f158c448e4c37f5c4d2ab8a77fc64
Jonathan Gomez <jo.gomez1111@gmail.com>
2026-01-01 16:28:24 -0300

    setup db models and alembic migrations

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from db.session import SessionLocal
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def healthcheck():
